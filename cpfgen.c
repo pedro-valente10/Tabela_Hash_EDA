dd8e97fe276c58fea86fd2361b59e56c9f25fe06
Henrique3391 <henrique.baumeier@gmail.com>
2026-05-13 20:36:56 -0300

    Refactor hash function and clean up main
    Refactor hash function to accept string input and remove duplicate main function.

--- cpfgen.c
+++ cpfgen.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 
+#include "simplemath.h"
 
 
+long hash(char * value) {
 
+  long converted = strtol(value, NULL, 10);
+  int result = 0;
+  int digits = count_digits(converted);
+  if (value[0] == '0') {
+    digits++;
+  }
+  int sum = long_sum(converted);
+  result = sum * first_digit(converted) * int_pow(converted, converted % 2 + converted % 3);
+  if (result < 1) {
+    result = result * -1;
+  }
+  return result;
 
-
-long hash(long value) {
-long result;
-int digits=0;
-long n=value;
-while (!(n==0)){
-n = n/10;
-digits++;
 }
-printf("digits is %i\n",digits);
 
-return result;
+int main(int argc, char * argv[]) {
 
+  if (argc < 2) {
+    printf("missing file arg.\n");
+    return 1;
+  }
+  printf(".\n");
+  FILE * file;
+  file = fopen(argv[1], "r");
+  char * ptr = "\n";
+  char buf[12];
+  long num;
+  if (file == NULL) {
+    printf("Unable to open file.\n");
+    return 1;
+  }
+  while (fscanf(file, "%s", buf) == 1) {
+    printf("%s : %li\n", buf, hash(buf));
+  }
+  //long test = 2;
+  //printf("%i\n",int_pow(test,5));
 
-}
-
-int main(int argc, char *argv[]) {
-
-if (argc<2) {
-printf("missing file arg.\n");
-return 1;
-}
-printf(".\n");
-FILE *file;
-file = fopen(argv[1],"r");
-char *ptr="\n";
-char buf[12];
-long num;
-if (file == NULL) {
-printf("Unable to open file.\n");
-return 1;
-}
-while(fscanf(file, "%s", buf)==1) {
- num = strtol(buf,NULL,10);
- printf("%s : %li\n",buf,hash(num));
-}
-printf("done\n");
-
-
-
-
-
+  printf("done\n");
 
 }
