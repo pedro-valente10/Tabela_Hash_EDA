57ecac1e87f8c6c1a7e0e8189d42799af199fd55
Henrique3391 <henrique.baumeier@gmail.com>
2026-05-13 21:14:32 -0300

    Add simplemath.h with mathematical function declarations

---
+++ simplemath.h
@@ -0,0 +1,11 @@
+#ifndef simplemath
+#define simplemath
+
+long int_pow(long base,int exponent);
+int count_digits(long number);
+int int_sum(int number);
+int long_sum(long number);
+int first_digit(long number);
+
+
+#endif
